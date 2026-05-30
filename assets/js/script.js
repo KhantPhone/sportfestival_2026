6e1356818ed6699ec104a00cbea17b15f4048e8c
KhantPhone <khantphone333@gmail.com>
2026-05-31 02:34:22 +0900

    responsive image fix and completed program section

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,29 +8,28 @@ document.querySelectorAll('.marquee__track').forEach(track => {
 
 
 //SWIPER
-	
 var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,        
+  centeredSlides: true,
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  breakpoints: {
+    768: {              
       slidesPerView: 2,
-      centeredSlides: true,
-      spaceBetween: 20,
-      grabCursor: true,
-      loop:true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-
-      },
-
-      loop:true,
-       autoplay: {
-        delay:1000,
-        disableOnInteraction: false,
-
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets : true,
-        
-      },
-    });
+    },
+    1024: {             
+      slidesPerView: 2,
+    }
+  }
+});
